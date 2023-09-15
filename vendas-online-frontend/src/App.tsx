046434fed6f919c84272722c587c614ef156a297
Cristiano <kristhian0.tj2019@gmail.com>
2023-09-15 13:29:37 -0300

    feat: insert notification

--- vendas-online-frontend/src/App.tsx
+++ vendas-online-frontend/src/App.tsx
@@ -1,4 +1,29 @@
-import LoginScreen from './modules/login';
+import type { Router as RemixRouter } from '@remix-run/router';
+import { RouteObject, RouterProvider, createBrowserRouter } from 'react-router-dom';
 
-function App() {return (<LoginScreen />)}
+import { loginRoutes } from './modules/login/routes';
+import { useNotification } from './shared/hooks/useNotification';
+
+const mainRoutes: RouteObject[] = [
+    {
+      path: '/',
+      element: <div>TELA PRINCIPAL</div>,
+      errorElement: <div>Página não encontrada!</div>
+    },
+  ];
+  
+  const router: RemixRouter = createBrowserRouter ([
+    ...mainRoutes,
+    ...loginRoutes,
+  ]);
+
+function App() {
+  const { contextHolder } = useNotification();
+  return (
+    <>
+      {contextHolder}
+      <RouterProvider router={router} />
+    </>
+  );
+}
 export default App;
--- vendas-online-frontend/src/main.tsx
+++ vendas-online-frontend/src/main.tsx
@@ -1,28 +1,16 @@
+import React from 'react';
 import './main.css';
-import type { Router as RemixRouter } from '@remix-run/router';
-import React from 'react'
+
+
 import ReactDOM from 'react-dom/client'
-import { RouterProvider, createBrowserRouter, RouteObject } from 'react-router-dom';
-import { loginRoutes } from './modules/login/routes';
+import App from './App';
 import { GlobalProvider } from './shared/hooks/useGlobalContext';
 
-const mainRoutes: RouteObject[] = [
-  {
-    path: '/',
-    element: <div>TELA PRINCIPAL</div>,
-    errorElement: <div>Página não encontrada!</div>
-  },
-];
-
-const router: RemixRouter = createBrowserRouter ([
-  ...mainRoutes,
-  ...loginRoutes,
-]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <GlobalProvider>
-      <RouterProvider router={router} />
+      <App />
     </GlobalProvider>
   </React.StrictMode>,
 );
