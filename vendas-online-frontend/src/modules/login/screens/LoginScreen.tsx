601806d54b03f7d288999235d94ed77381d4e8cf
Cristiano <kristhian0.tj2019@gmail.com>
2023-09-14 21:10:29 -0300

    feat: change layout and create button component

--- vendas-online-frontend/src/modules/login/screens/LoginScreen.tsx
+++ vendas-online-frontend/src/modules/login/screens/LoginScreen.tsx
@@ -1,5 +1,6 @@
+import Button from "../../../shared/buttons/button/Button";
 import Input from "../../../shared/inputs/input/Input";
-import { BackgroundImage, ContainerLogin, ContainerLoginScreen, LimitedContainer, LogoImage } from "../styles/loginScreen.styles";
+import { BackgroundImage, ContainerLogin, ContainerLoginScreen, LimitedContainer, LogoImage, TitleLogin } from "../styles/loginScreen.styles";
 
 const LoginScreen = () => {
     return (<div>
@@ -7,8 +8,10 @@ const LoginScreen = () => {
         <ContainerLogin>
           <LimitedContainer>
             <LogoImage src="./logo.png" />
+            <TitleLogin level={2} type="secondary" >LOGIN</TitleLogin>
             <Input title="USUÁRIO"/>
             <Input title="SENHA"/>
+            <Button type="primary" margin="34px 0px 16px 0px">ENTRAR</Button>
           </LimitedContainer>
         </ContainerLogin>
         <BackgroundImage src="./background.png"/>
