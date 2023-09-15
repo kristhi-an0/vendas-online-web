e1db6b24439c40e5bfe4e544c24db220a513c465
Cristiano <kristhian0.tj2019@gmail.com>
2023-09-14 22:36:03 -0300

    feat: insert axios and first request

--- vendas-online-frontend/src/modules/login/screens/LoginScreen.tsx
+++ vendas-online-frontend/src/modules/login/screens/LoginScreen.tsx
@@ -2,23 +2,34 @@ import { useState } from "react";
 import Button from "../../../shared/buttons/button/Button";
 import Input from "../../../shared/inputs/input/Input";
 import { BackgroundImage, ContainerLogin, ContainerLoginScreen, LimitedContainer, LogoImage, TitleLogin } from "../styles/loginScreen.styles";
+import axios from "axios";
 
 const LoginScreen = () => {
-  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  const handleUsername = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setUsername(event.target.value);
+  const handleEmail = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
   };
   const handlePassword = (event: React.ChangeEvent<HTMLInputElement>) => {
     setPassword(event.target.value);
   };
 
-  const handleLogin = () => {
-    alert(`
-      username: ${username},
-      password: ${password}
-    `);
+  const handleLogin = async () => {
+    await axios({
+      method: 'post',
+      url: 'http://localhost:3000/auth',
+      data: {
+        email: email,
+        password: password,
+      },
+    }).then((result) => {
+      alert(`Fez login ${result.data.accessToken}`);
+      return result.data;
+    }).catch(() => {
+      alert('Usuário ou senha inválido.');
+    });
+    alert(`email: ${email}, password: ${password}`);
   };
 
   return (<div>
@@ -27,7 +38,7 @@ const LoginScreen = () => {
         <LimitedContainer>
           <LogoImage src="./logo.png" />
           <TitleLogin level={2} type="secondary" >LOGIN</TitleLogin>
-          <Input title="USUÁRIO" margin="32px 0px 0px" onChange={handleUsername} value={username} />
+          <Input title="USUÁRIO" margin="32px 0px 0px" onChange={handleEmail} value={email} />
           <Input type="password" title="SENHA" margin="32px 0px 0px" onChange={handlePassword} value={password}/>
           <Button type="primary" margin="34px 0px 16px 0px" onClick={handleLogin}>ENTRAR</Button>
         </LimitedContainer>
